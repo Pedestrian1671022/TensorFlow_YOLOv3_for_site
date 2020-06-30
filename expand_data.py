56ecc9480b46072689815bdd02d1d1de5b6b0121
Pedestrian刘鑫 <1172012891@qq.com>
2020-06-30 08:56:48 +0800

    Update expand_data.py

--- expand_data.py
+++ expand_data.py
@@ -1,4 +1,3 @@
-
 import os
 import cv2
 import math
@@ -7,10 +6,7 @@ import colorsys
 import numpy as np
 
 
-def random_crop(image_path, image, bboxes):
-    width = 1024
-    height = 1024
-    padding = 5
+def random_crop(width, height, padding, image_path, image, bboxes):
     h, w, _ = image.shape
     max_bbox = np.concatenate([np.min(bboxes[:, 0:2], axis=0), np.max(bboxes[:, 2:4], axis=0)], axis=-1)
 
@@ -164,22 +160,67 @@ def perspective_translate(image, bboxes):
 
 def mixup(image, bboxes):
     if random.random() < 0.5:
-        annot_path = "/home/pc405/Music/Pedestrian/TensorFlow_YOLOv3_for_site/LabelImage_v1.8.1/data/train.txt"
+        annot_path = "/home/Pedestrian/Documents/TensorFlow_YOLOv3_for_site/LabelImage_v1.8.1/data/train.txt"
         annotations = load_annotations(annot_path)
         image1, bboxes1 = image, bboxes
         index = random.randint(0, len(annotations)-1)
         line = annotations[index].split()
-        image_path = line[0] + " " + line[1]
+        image_path = line[0]
         if not os.path.exists(image_path):
             raise KeyError("%s does not exist ... " % image_path)
         image2 = np.array(cv2.imread(image_path))
-        bboxes2 = np.array([list(map(int, box.split(','))) for box in line[2:]])
-        image2, bboxes2 = random_crop(image_path, np.copy(image2), np.copy(bboxes2))
+        bboxes2 = np.array([list(map(int, box.split(','))) for box in line[1:]])
+        image2, bboxes2 = random_crop(1024, 1024, 5, image_path, np.copy(image2), np.copy(bboxes2))
         image2, bboxes2 = random_horizontal_flip(np.copy(image2), np.copy(bboxes2))
         alpha = random.uniform(0.3, 0.7)
         beta = random.uniform(0.3, 0.7)
         image = cv2.addWeighted(image1, alpha, image2, beta, 0)
         bboxes = np.vstack((bboxes1, bboxes2))
+    return image, bboxes
+
+def mosaic(image, bboxes):
+    if random.random() < 0.5:
+        annot_path = "/home/Pedestrian/Documents/TensorFlow_YOLOv3_for_site/LabelImage_v1.8.1/data/train.txt"
+        annotations = load_annotations(annot_path)
+        image1, bboxes1 = random_crop(512, 512, 0, "mosaic", image, bboxes)
+
+        index2 = random.randint(0, len(annotations)-1)
+        line2 = annotations[index2].split()
+        image_path2 = line2[0]
+        if not os.path.exists(image_path2):
+            raise KeyError("%s does not exist ... " % image_path2)
+        image2 = np.array(cv2.imread(image_path2))
+        bboxes2 = np.array([list(map(int, box.split(','))) for box in line2[1:]])
+        image2, bboxes2 = random_crop(512, 512, 5, image_path2, np.copy(image2), np.copy(bboxes2))
+        image2, bboxes2 = random_horizontal_flip(np.copy(image2), np.copy(bboxes2))
+        bboxes2[:, [1, 3]] = bboxes2[:, [1, 3]] + 512
+
+        index3 = random.randint(0, len(annotations) - 1)
+        line3 = annotations[index3].split()
+        image_path3 = line3[0]
+        if not os.path.exists(image_path3):
+            raise KeyError("%s does not exist ... " % image_path3)
+        image3 = np.array(cv2.imread(image_path3))
+        bboxes3 = np.array([list(map(int, box.split(','))) for box in line3[1:]])
+        image3, bboxes3 = random_crop(512, 512, 5, image_path3, np.copy(image3), np.copy(bboxes3))
+        image3, bboxes3 = random_horizontal_flip(np.copy(image3), np.copy(bboxes3))
+        bboxes3[:, [0, 2]] = bboxes3[:, [0, 2]] + 512
+
+        index4 = random.randint(0, len(annotations) - 1)
+        line4 = annotations[index4].split()
+        image_path4 = line4[0]
+        if not os.path.exists(image_path4):
+            raise KeyError("%s does not exist ... " % image_path4)
+        image4 = np.array(cv2.imread(image_path4))
+        bboxes4 = np.array([list(map(int, box.split(','))) for box in line4[1:]])
+        image4, bboxes4 = random_crop(512, 512, 5, image_path4, np.copy(image4), np.copy(bboxes4))
+        image4, bboxes4 = random_horizontal_flip(np.copy(image4), np.copy(bboxes4))
+        bboxes4[:, [0, 1, 2, 3]] = bboxes4[:, [0, 1, 2, 3]] + 512
+
+        image_1 = np.concatenate((image1, image2), axis=0)
+        image_2 = np.concatenate((image3, image4), axis=0)
+        image = np.concatenate((image_1, image_2), axis=1)
+        bboxes = np.vstack((bboxes1, bboxes2, bboxes3, bboxes4))
     return image, bboxes
 
 
@@ -200,14 +241,15 @@ def parse_annotation(annotation):
     image = np.array(cv2.imread(image_path))
     bboxes = np.array([list(map(int, box.split(','))) for box in line[1:]])
 
-    image, bboxes = random_crop(image_path, np.copy(image), np.copy(bboxes))
+    image, bboxes = random_crop(1024, 1024, 5, image_path, np.copy(image), np.copy(bboxes))
     image, bboxes = random_horizontal_flip(np.copy(image), np.copy(bboxes))
     # image, bboxes = random_erasing(np.copy(image), np.copy(bboxes))
     # image, bboxes = hide_patch(np.copy(image), np.copy(bboxes))
     # image, bboxes = grid_mask(np.copy(image), np.copy(bboxes))
     # image, bboxes = affine_translate(np.copy(image), np.copy(bboxes))
     # image, bboxes = perspective_translate(np.copy(image), np.copy(bboxes))
-    image, bboxes = mixup(np.copy(image), np.copy(bboxes))
+    # image, bboxes = mixup(np.copy(image), np.copy(bboxes))
+    image, bboxes = mosaic(np.copy(image), np.copy(bboxes))
     return image, bboxes
 
 
@@ -221,7 +263,7 @@ def read_class_names(class_file_name):
 
 
 def draw_bbox(image, bboxes, classes=read_class_names(
-    "/home/pc405/Music/Pedestrian/TensorFlow_YOLOv3_for_site/LabelImage_v1.8.1/data/predefined_classes.txt"),
+    "/home/Pedestrian/Documents/TensorFlow_YOLOv3_for_site/LabelImage_v1.8.1/data/predefined_classes.txt"),
               show_label=True):
 
     num_classes = len(classes)
@@ -255,7 +297,7 @@ def draw_bbox(image, bboxes, classes=read_class_names(
 
 
 if __name__ == '__main__':
-    annot_path = "/home/pc405/Music/Pedestrian/TensorFlow_YOLOv3_for_site/LabelImage_v1.8.1/data/train.txt"
+    annot_path = "/home/Pedestrian/Documents/TensorFlow_YOLOv3_for_site/LabelImage_v1.8.1/data/train.txt"
     annotations = load_annotations(annot_path)
     for annotation in annotations:
         image, bboxes = parse_annotation(annotation)
