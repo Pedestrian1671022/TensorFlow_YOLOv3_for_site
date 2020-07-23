b92318ea38b8e994449af11b7b40d54ef118a52e
Pedestrian刘鑫 <1172012891@qq.com>
2020-07-23 08:38:24 +0800

    Add files via upload

---
+++ print_pb_tensorname.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import os
+
+model_dir = './'
+model_name = 'model_data/mars-small128.pb'
+
+
+def create_graph():
+    with tf.gfile.GFile(os.path.join(
+            model_dir, model_name), 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        tf.import_graph_def(graph_def, name='')
+
+
+create_graph()
+tensor_name_list = [tensor.name for tensor in tf.get_default_graph().as_graph_def().node]
+for tensor_name in tensor_name_list:
+    print(tensor_name, '\n')
